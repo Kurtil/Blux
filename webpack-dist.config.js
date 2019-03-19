43d063ae393db1cda3443f8840c1ac4bb8752846
g.lagier <gaetan.lagier@hotmail.fr>
2019-03-19 21:29:34 +0100

    add dummy circle ci config file to prevent test failure BIS

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -32,6 +32,7 @@ module.exports = {
     new CopyPlugin([
       { from: 'assets/', to: 'assets/' },
       { from: 'index.dist.html', to: 'index.html' },
+      { from: '.circleci/dummy_gh-pages.config.yml', to: '.circleci/config.yml'},
     ]),
   ],
 };
